40d9e95d40a17bb2413ccdc5d7d9fee7a2be5c72
rogeraabbccdd <rogeraabbccdd@gmail.com>
2024-08-01 10:01:15 +0800

    1.0.0

---
+++ C4ModelChanger.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.Json.Serialization;
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Utils;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+
+namespace C4ModelChangerPlugin;
+
+public class Config : BasePluginConfig
+{
+    [JsonPropertyName("ConfigVersion")]
+    public override int Version { get; set; } = 1;
+
+    [JsonPropertyName("Model")]
+    public string Model { get; set; } = "";
+
+    [JsonPropertyName("Size")]
+    public float Scale { get; set; } = 1.0f;
+}
+
+public class Plugin : BasePlugin, IPluginConfig<Config>
+{
+    public override string ModuleName => "C4 Model Changer";
+    public override string ModuleVersion => "1.0.0";
+    public override string ModuleAuthor => "Kento";
+    public override string ModuleDescription => "Change C4 Model.";
+    public Config Config { get; set; } = new();
+
+    public override void Load(bool hotReload)
+    {
+        RegisterListener<Listeners.OnServerPrecacheResources>(OnPrecacheResources);
+    }
+    public override void Unload(bool hotReload)
+    {
+        RemoveListener<Listeners.OnServerPrecacheResources>(OnPrecacheResources);
+    }
+    public void OnConfigParsed(Config config)
+    {
+        Config = config;
+    }
+    public void OnPrecacheResources (ResourceManifest manifest)
+    {
+        manifest.AddResource(Config.Model);
+    }
+
+    [GameEventHandler]
+    public HookResult OnEventBombPlanted(EventBombPlanted @event, GameEventInfo info)
+    {
+        var c4 = Utilities.FindAllEntitiesByDesignerName<CPlantedC4>("planted_c4").First();
+        if (c4 == null || c4.Entity == null || !c4.IsValid) return HookResult.Continue;
+
+        if (Config.Model.Length == 0)   return HookResult.Continue;
+        c4.SetModel(Config.Model);
+        c4.Entity.EntityInstance.AddEntityIOEvent("SetScale", null, null, Config.Scale.ToString());
+
+        return HookResult.Continue;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# CS2-C4ModelChanger
+# CS2 C4 Model Changer
+Change C4 model.
+  
+![screenshot](./screenshot.jpg)
+
+## Install
+- Upload all files to `/game/csgo/addons/counterstrikesharp/`.
+
+## Configs
+|Name|Description|Default|
+|---|---|---|
+|Model|Model to change||
+|Size|Model size|1|
